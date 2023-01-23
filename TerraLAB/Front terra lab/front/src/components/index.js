c282dde5658c827fcd157145465395134d3d1daa
NiquiniNiwi <niquini71@gmail.com>
2023-01-23 10:08:18 -0300

    removed terralab folder

--- TerraLAB/Front terra lab/front/src/components/index.js
+++
@@ -1,8 +0,0 @@
-
-export { default as Navigation } from "./Navigation";
-export { default as Footer } from "./Footer";
-export { default as Home } from "./Home";
-export { default as Sobre } from "./Sobre";
-export { default as Contato } from "./Contato";
-export { default as Cadastro } from "./Cadastro";
-export { default as Login } from "./Login";
--- TerraLAB/Front terra lab/front/src/services/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-    baseURL: 'https://release-cavemaps.herokuapp.com'
-});
-
-export default api;
